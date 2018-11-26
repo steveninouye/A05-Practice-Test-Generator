34fe7290ba2872efc1fc2fd957c8a92aadd004d1
Steven Inouye <steveninouye@msn.com>
2018-11-25 17:42:43 -0800

    Fixe test and remove all bugs

--- practice_test.js
+++ practice_test.js
@@ -106,6 +106,7 @@ Array.prototype.twoSum = function() {
 };
 
 Array.prototype.rotate = function(num = 1) {
+   console.log(this);
    let mod = num % this.length;
    if (mod >= 0) {
       return this.slice(mod).concat(this.slice(0, mod));
@@ -114,142 +115,54 @@ Array.prototype.rotate = function(num = 1) {
    }
 };
 
-function transpose(arr) {
-   let result = [];
-   arr.forEach((array, idx1) => {
-      array.forEach((el, idx2) => {
-         if (idx1 === 0) result.push([]);
-         result[idx2].push(el);
-      });
-   });
-   return result;
-}
+function transpose(arr) {}
 
-Array.prototype.flatten = function() {
-   return this.reduce(
-      (ac, el) =>
-         el instanceof Array ? ac.concat(el.flatten()) : ac.concat([el]),
-      []
-   );
-};
+Array.prototype.flatten = function() {};
 
 // Write a method that returns the factors of a number in ascending order.
 
-function factors(num) {
-   let result = [];
-   for (let i = 1; i <= num; i++) {
-      if (num % i === 0) {
-         result.push(i);
-      }
-   }
-   return result;
-}
+function factors(num) {}
 
 // primes(num) returns an array of the first "num" primes.
 // You may wish to use an is_prime helper method.
 
-function primes(num) {
-   let result = [];
-   for (let i = 2; result.length < num; i++) {
-      if (isPrime(i)) {
-         result.push(i);
-      }
-   }
-   return result;
-}
-
-function isPrime(num) {
-   for (let i = 2; i <= Math.ceil(num / 2); i++) {
-      if (num % i === 0) return false;
-   }
-   return true;
-}
+function primes(num) {}
 
 // write String.prototype.mySlice. It should take a start index and an
 // (optional) end index.
 
-String.prototype.mySlice = function(start, end) {
-   if (start > end) return '';
-   end = end || this.length;
-   let result = '';
-   for (let i = start; i < end; i++) {
-      if (this[i]) result += this[i];
-   }
-   return result;
-};
+String.prototype.mySlice = function() {};
 
 // Write a recursive method that returns all of the permutations of an array
 
-function permutations(array) {
-   if (array.length <= 1) return [array];
-   let el = array.pop();
-   let prev = permutations(array);
-   let result = [];
-   prev.forEach((e) => {
-      for (let i = 0; i <= prev.length; i++) {
-         let temp = e.slice();
-         temp.splice(i, 0, el);
-         result.push(temp);
-      }
-   });
-   return result.sort();
-}
+function permutations(array) {}
 
 // Write a recursive method that returns the sum of all elements in an array
 
-function recSum(nums) {
-   if (nums.length === 0) return 0;
-   let el = nums.pop();
-   return el + recSum(nums);
-}
+function recSum(nums) {}
 
 // Using recursion and the is_a? method,
 // write an Array#deep_dup method that will perform a "deep" duplication of the interior arrays.
 
-function deepDup(arr) {
-   return arr.map((el) => (el instanceof Array ? deepDup(el) : el));
-}
+function deepDup(arr) {}
 
 // return the sum of the first n even numbers recursively. Assume n > 0
 
-function firstEvenNumbersSum(n) {
-   if (n === 0) return 0;
-   return 2 * n + firstEvenNumbersSum(n - 1);
-}
+function firstEvenNumbersSum(n) {}
 
 // return b^n recursively. Your solution should accept negative values
 // for n
 
-function exponent(b, n) {
-   if (n === 0) return 1;
-   if (n > 0) {
-      return b * exponent(b, n - 1);
-   } else {
-      return (1 / b) * exponent(b, n + 1);
-   }
-}
+function exponent(b, n) {}
 
 // returns all subsets of an array
 
-function subsets(array) {
-   if (array.length === 0) return [[]];
-   let el = array.pop();
-   let prev = subsets(array);
-   let dup = prev.slice();
-   dup = dup.map((e) => e.slice().concat([el]));
-   return prev.concat(dup);
-}
+function subsets(array) {}
 
 // Implement a method that finds the sum of the first n
 // fibonacci numbers recursively. Assume n > 0
 
-function fibsSum(n) {
-   if (n === 0) return 0;
-   if (n === 1) return 1;
-   let prev = fibsSum(n - 1);
-   let prev2 = fibsSum(n - 2);
-   return prev + prev2 + 1;
-}
+function fibsSum(n) {}
 
 // Write a method, `digital_root(num)`. It should Sum the digits of a positive
 // integer. If it is greater than 10, sum the digits of the resulting number.
@@ -259,15 +172,7 @@ function fibsSum(n) {
 // You may wish to use a helper function, `digital_root_step(num)` which performs
 // one step of the process.
 
-function digitalRoot(num) {
-   if (num < 10) return num;
-   let result = 0;
-   while (num > 0) {
-      result += num % 10;
-      num = Math.floor(num / 10);
-   }
-   return digitalRoot(result);
-}
+function digitalRoot(num) {}
 
 // Write a recursive method that takes in a base 10 number n and
 // converts it to a base b number. Return the new number as a string
@@ -275,27 +180,14 @@ function digitalRoot(num) {
 // E.g. base_converter(5, 2) == "101"
 // base_converter(31, 16) == "1f"
 
-function baseConverter(num, b) {
-   if (num === 0) return '';
-   let letters = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
-   return baseConverter(Math.floor(num / b), b) + letters[num % b];
-}
+function baseConverter(num, b) {}
 
 // Write a recursive function that returns the prime factorization of
 // a given number. Assume num > 1
 //
 // prime_factorization(12) => [2,2,3]
 
-function primeFactorization(num) {
-   let result = [];
-   for (let i = 2; num > 1; i++) {
-      while (num % i === 0) {
-         result.push(i);
-         num /= i;
-      }
-   }
-   return result;
-}
+function primeFactorization(num) {}
 
 // Write a recursive method that takes in a string to search and a key string.
 // Return true if the string contains all of the characters in the key
@@ -304,104 +196,25 @@ function primeFactorization(num) {
 // string_include_key?("cadbpc", "abc") => true
 // string_include_key("cba", "abc") => false
 
-function stringIncludeKey(string, key) {
-   if (key.length === 0) return true;
-   let ltr = key[0];
-   let idx = string.indexOf(ltr);
-   if (idx === -1) {
-      return false;
-   } else {
-      return stringIncludeKey(string.slice(idx + 1), key.slice(1));
-   }
-}
+function stringIncludeKey(string, key) {}
 
 // Write a recursive method that returns the first "num" factorial numbers.
 // Note that the 1st factorial number is 0!, which equals 1. The 2nd factorial
 // is 1!, the 3rd factorial is 2!, etc.
 
-function factorialsRec(num) {
-   if (num === 1) {
-      return [1];
-   }
-   let result = 1;
-   for (let i = 1; i < num; i++) {
-      result *= i;
-   }
-   return factorialsRec(num - 1).concat([result]);
-}
+function factorialsRec(num) {}
 
-Array.prototype.bubbleSort = function(func) {
-   func =
-      func ||
-      function(x, y) {
-         if (x > y) return 1;
-         return -1;
-      };
-   let dup = this.slice();
-   let sorted = false;
-   while (!sorted) {
-      sorted = true;
-      dup.forEach((el1, i) => {
-         if (i !== dup.length - 1) {
-            let j = i + 1;
-            if (func(el1, dup[j]) === 1) {
-               [dup[i], dup[j]] = [dup[j], dup[i]];
-               sorted = false;
-            }
-         }
-      });
-   }
-   return dup;
-};
+Array.prototype.bubbleSort = function(func) {};
 
 // Write an Array#merge_sort method; it should not modify the original array.
 
-Array.prototype.mergeSort = function(func) {
-   if (this.length <= 1) return this;
-   func =
-      func ||
-      function(x, y) {
-         if (x > y) return 1;
-         return -1;
-      };
-   let midpt = Math.floor(this.length / 2);
-   let left = this.slice(0, midpt).mergeSort(func);
-   let right = this.slice(midpt).mergeSort(func);
-   return left.merge(right, func);
-};
+Array.prototype.mergeSort = function(func) {};
 
-Array.prototype.merge = function(arr, func) {
-   let result = [];
-   while (this.length > 0 && arr.length > 0) {
-      if (func(this[0], arr[0]) === 1) {
-         result.push(arr.shift());
-      } else {
-         result.push(this.shift());
-      }
-   }
-   return result.concat(this).concat(arr);
-};
+Array.prototype.merge = function(arr, func) {};
 
 // Write a monkey patch of quick sort that accepts a callback
 
-Array.prototype.quickSort = function(func) {
-   let dup = this.slice();
-   if (dup.length <= 1) return dup;
-   func =
-      func ||
-      function(x, y) {
-         if (x > y) return 1;
-         return -1;
-      };
-   let midpt = Math.floor(dup.length / 2);
-   let element = dup.shift();
-   let left = dup.filter((el) => func(element, el) === 1);
-   let right = dup.filter((el) => func(element, el) !== 1);
-   return left
-      .quickSort(func)
-      .concat([element])
-      .concat(right.quickSort(func));
-};
+Array.prototype.quickSort = function(func) {};
 
 // Jumble sort takes a string and an alphabet. It returns a copy of the string
 // with the letters re-ordered according to their positions in the alphabet. If
@@ -411,29 +224,12 @@ Array.prototype.quickSort = function(func) {
 // jumble_sort("hello") => "ehllo"
 // jumble_sort("hello", ['o', 'l', 'h', 'e']) => 'ollhe'
 
-function jumbleSort(str, alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')) {
-   str = str.split('');
-   return alphabet.reduce(
-      (acc, ltr) => acc + str.filter((el) => el === ltr).join(''),
-      ''
-   );
-}
+function jumbleSort(str, alphabet = null) {}
 
 // Write a method that capitalizes each word in a string like a book title
 // Do not capitalize words like 'a', 'and', 'of', 'over' or 'the'
 
-function titleize(title) {
-   let words = ['a', 'and', 'of', 'over', 'the'];
-   title = title.split(' ');
-   return title
-      .reduce((acc, word, i) => {
-         if (!words.includes(word) || i === 0) {
-            word = word[0].toUpperCase() + word.slice(1);
-         }
-         return acc.concat([word]);
-      }, [])
-      .join(' ');
-}
+function titleize(title) {}
 
 // Back in the good old days, you used to be able to write a darn near
 // uncrackable code by simply taking each letter of a message and incrementing it
@@ -445,75 +241,25 @@ function titleize(title) {
 // To get an array of letters "a" to "z", you may use `("a".."z").to_a`. To find
 // the position of a letter in the array, you may use `Array#find_index`.
 
-function caesarCipher(str, shift) {
-   let letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-   return str.split('').reduce((acc, ltr) => {
-      let idx = letters.indexOf(ltr);
-      if (idx === -1) {
-         return acc + ltr;
-      } else if (idx + shift > 25) {
-         return acc + letters[idx + shift - 26];
-      } else {
-         return acc + letters[idx + shift];
-      }
-   }, '');
-}
+function caesarCipher(str, shift) {}
 
 // Write a String#symmetric_substrings method that returns an array of substrings
 // that are palindromes, e.g. "cool".symmetric_substrings => ["oo"]
 // Only include substrings of length > 1.
 
-String.prototype.symmetricSubstrings = function() {
-   let result = [];
-   for (let i = 0; i < this.length - 1; i++) {
-      for (let j = i + 2; j < this.length + 1; j++) {
-         let word = this.slice(i, j);
-
-         if (isPalindrome(word)) {
-            result.push(word);
-         }
-      }
-   }
-   return result.sort();
-};
-
-function isPalindrome(str) {
-   str2 = str.split('').slice();
-   return str.split('').join('') === str2.reverse().join('');
-}
+String.prototype.symmetricSubstrings = function() {};
 
 // Returns an array of all the subwords of the string that appear in the
 // dictionary argument. The method does NOT return any duplicates.
 
-String.prototype.realWordsInString = function(dictionary) {
-   return dictionary.reduce((acc, word) => {
-      if (this.indexOf(word) !== -1) {
-         acc.push(word);
-      }
-      return acc;
-   }, []);
-};
+String.prototype.realWordsInString = function(dictionary) {};
 
 // Write a method that translates a sentence into pig latin. You may want a helper method.
 // 'apple' => 'appleay'
 // 'pearl' => 'earlpay'
 // 'quick' => 'ickquay'
 
-function pigLatinify(sentence) {
-   let vowels = 'aeiou'.split('');
-   let words = sentence.split(' ');
-   return words
-      .reduce((acc, word) => {
-         while (!vowels.includes(word[0])) {
-            word = word.slice(1) + word.slice(0, 1);
-         }
-         if (word[0] === 'u' && word[-1] === 'q') {
-            word = word.slice(1) + word.slice(0, 1);
-         }
-         return acc.concat([`${word}ay`]);
-      }, [])
-      .join(' ');
-}
+function piglatinify(sentence) {}
 
 // Write a method that doubles each element in an array
 
